924ad3dd1db8a2302ab391348de1776c3522a615
Alexey Nikitin <bohr@narod.ru>
2012-08-21 03:09:52 +0400

    belongs_to_date? method

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,24 @@ class Event < ActiveRecord::Base
   has_many :months
   has_many :month_days
   has_many :week_days
+
+  def belongs_to_date? date
+    if !years.empty? && years.detect{|y| y.year == date.year}.nil?
+        return false
+    end
+
+    if !months.empty? && months.detect{|m| m.month == date.month}.nil?
+        return false
+    end
+
+    if !month_days.empty? && month_days.detect{|md| md.day == date.day}.nil?
+        return false
+    end
+
+    if !week_days.empty? && week_days.detect{|wd| wd.day == date.wday}.nil?
+        return false
+    end
+
+    true
+  end
 end
