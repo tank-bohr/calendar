d6aa3fc16d7acdb524b2d67e3869c15ad680efd0
Алексей Никитин <xoma@xoma-eeepc.(none)>
2012-08-21 00:32:13 +0400

    new empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendar::Application.config.secret_token = '1e5bc43f27148f590aa9422f4590ce45d54a1da18202b4d078cd2a71eabce5e1fec7f08033a829859b0878543443bdfb21d1bc243e40e7178247ff7069aa53f7'
