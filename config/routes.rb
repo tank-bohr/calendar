27cb7b9d3deefb84758ba33cdf9d6bca1dd25c72
Alexey Nikitin <xoma@lenta-co.ru>
2012-08-21 19:12:42 +0400

    add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Calendar::Application.routes.draw do
+  get "welcome/index"
+
   get "event/new"
   post "event/create"
 
@@ -55,7 +57,7 @@ Calendar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
