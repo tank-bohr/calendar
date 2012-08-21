d769d6d8475f11ebcdea1f276ac2f4e64a285067
Alexey Nikitin <bohr@narod.ru>
2012-08-21 04:24:11 +0400

    add weekly method

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,11 +30,29 @@ class Event < ActiveRecord::Base
 
 
   def add_weekly str
+    days = str.split /\s*\,\s*/
+    week_days_numbers = {mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6, sun: 7}
+    p week_days_numbers
+
+    processed = days.map do |d|
+        digit = nil
+        if d =~ /^\d$/
+            digit = d.to_i
+        elsif d =~ /(\w+)/
+            word = $1
+            digit = week_days_numbers[word.downcase.to_sym]
+        end
+        digit
+    end.compact.uniq
+
+    self.week_days = processed.map{|day| WeekDay.new :day => day}
+
+    self.save
   end
 
   def add_monthly str
     days = str.split /\s*\,\s*/
-    self.month_days = days.map{|d| MonthDay.new :day => d}
+    self.month_days = days.map{|d| MonthDay.new :day => d}.uniq
     self.save
   end
 end
